430d750f30b8897fb262a01da73d0eca051b7c95
Nate Ladd <62190822+nathanladd@users.noreply.github.com>
2025-10-25 19:10:39 -0600

    clean up buttons by type

--- domain/constants.py
+++ domain/constants.py
@@ -1,37 +1,38 @@
 
 from domain.snaptypes import SnapType
 
-
+# Application title global
 APP_TITLE = "Snapshot Decoder"
 
-
+# Buttons for each snapshot type
+# Button name, COMMAND NAME, tooltip
 BUTTONS_BY_TYPE: dict[SnapType, list[tuple[str, str, str]]] = {
     SnapType.ECU_V1: [
         ("Battery Test", "V1_BATTERY_TEST", "Plot battery V vs RPM"),
         ("Rail Pressure", "V1_RAIL_PRESSURE", "Demand vs Actual + Gap"),
-        ("IMV Test", "imv_test", "Demand vs Feedback"),
-        ("Injector Balance", "inj_balance", "Delta speed per cylinder"),
-        ("Start Health", "start_health", "Cranking RPM, V, rail build"),
+        
     ],
     SnapType.ECU_V2: [
-        ("Start Health", "start_health", "Cranking RPM, V, rail build"),
-        ("Boost Check", "boost_check", "MAP vs Atmosphere"),
-        ("EGR Position", "egr_position", "Cmd vs Feedback"),
+        ("Battery Test", "V2_BATTERY_TEST", "Plot battery V vs RPM"),
+        
     ],
     SnapType.DCU_V1: [
-        ("Usage Summary", "usage_summary", "Key hours, PTO, load"),
-        ("Min/Max Chart", "minmax_chart", "Voltage & RPM extremes"),
+        
     ],
     SnapType.EUD_V1: [
-        ("I/O Monitor", "io_monitor", "Digital/analog channels"),
-        ("Fault Timeline", "fault_timeline", "DTCs over time"),
+        
+    ],
+    SnapType.EUD_V2: [
+        
     ],
 }
 
-# Standardize the labels found in the header. - labels we expect in row 0..3, col 0, with values in col 1.
+# Standardize the labels found in the header. 
+# - labels we expect in row 0..3 of collumn 0, with values in collumn 1.
+# squeez and clean the name from the snapshot cell and map it to a more readable name
 HEADER_LABELS = {
     "engine model": "Engine Model",
     "ecu map version": "ECU Map Version",
-    "program sw version": "Program SW Version",
+    "program sw version": "Engine Analyzer Version",
     "data logging": "Data Logging"
 }
